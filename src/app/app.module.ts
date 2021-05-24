055053319c94f62b805c49a96668eb6b15078b27
roman_roman <pelipec1980@gmail.com>
2021-05-24 10:35:26 +0300

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,31 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
+import { TodoMenuComponent } from './components/todo-menu/todo-menu.component';
+import { TodoTextComponent } from './fragments/todo-text/todo-text.component';
+import { SearchComponent } from './fragments/search/search.component';
+import { RemainingComponent } from './fragments/remaining/remaining.component';
+import { TodoListComponent } from './fragments/todo-list/todo-list.component';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import { CreateTaskComponent } from './fragments/create-task/create-task.component';
+import { CleanElementsComponent } from './fragments/clean-elements/clean-elements.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoMenuComponent,
+    TodoTextComponent,
+    SearchComponent,
+    RemainingComponent,
+    TodoListComponent,
+    CreateTaskComponent,
+    CleanElementsComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule,
+    FormsModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
